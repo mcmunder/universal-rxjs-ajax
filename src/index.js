d443584954b309754d9c54d2e2e10607b5231831
Tomasz Mularczyk <tomasz.mularczyk@risendot.com>
2018-01-18 15:32:19 +0100

    use direct import for ajax to avoid bundling whole library. Make rxjs a peer dependency.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import {Observable} from 'rxjs'
+import { ajax } from 'rxjs/observable/dom/ajax'
 import xhr2 from 'xhr2'
 
 const XHR2 = typeof XMLHttpRequest !== 'undefined'
@@ -6,5 +6,5 @@ const XHR2 = typeof XMLHttpRequest !== 'undefined'
   : xhr2
 
 export const request = function request(options) {
-  return Observable.ajax({createXHR: () => new XHR2(), ...options})
+  return ajax({createXHR: () => new XHR2(), ...options})
 }
