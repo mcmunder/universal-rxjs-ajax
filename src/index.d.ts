70e67c2a569be4180511e5d820dacffde37e5006
Andrew Crites <acrites@mobiquityinc.com>
2018-02-14 11:51:05 -0500

    feat(types): Adding types

---
+++ src/index.d.ts
@@ -0,0 +1,4 @@
+import { AjaxRequest, AjaxResponse } from 'rxjs/observable/dom/AjaxObservable';
+import { Observable } from 'rxjs/Observable';
+
+export function request(options: AjaxRequest): Observable<AjaxResponse>;
