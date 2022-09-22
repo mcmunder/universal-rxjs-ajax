1d23dfd15532d434f49da1997c626b0fdcd365a6
Matthias Munder <matthias.munder@staffbase.com>
2022-09-22 15:47:46 +0200

    resolve merge conflict

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,6 @@ import xhr2 from 'xhr2'
 
 const XHR2 = typeof XMLHttpRequest !== 'undefined' ? XMLHttpRequest : xhr2
 
-<<<<<<< HEAD
-export const request = function request<T>(
-  config: AjaxConfig
-): Observable<AjaxResponse<T>> {
-=======
 export function request<T>(config: AjaxConfig): Observable<AjaxResponse<T>> {
->>>>>>> e6e5100 (project revamp)
   return ajax<T>({createXHR: () => new XHR2(), ...config})
 }
